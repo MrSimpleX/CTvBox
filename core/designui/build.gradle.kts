9369c12836a203900bf4939d6d79fd7ccc422c0d
zhangsq <zhangshuoqing@niuguwang.com>
2026-04-14 14:35:39 +0800

    feat: 同步代码

--- core/designui/build.gradle.kts
+++ core/designui/build.gradle.kts
@@ -9,6 +9,7 @@ android {
 
 dependencies {
     api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.material.iconsExtended)
 
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.io.insert.koin.core)
